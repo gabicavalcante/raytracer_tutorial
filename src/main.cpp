9764e3da77bce7e0860f7aaae18bd09db5b3c38a
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-02-24 15:34:46 -0300

    Multiple Objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-#include "ray.h"
+#include "float.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
@@ -23,19 +25,21 @@ float hit_sphere(const vec3 &center, float radius, const ray &r)
 // t = 1.0 -> blue
 // t = 0.0 -> white
 // linear_interpolation: blendedValue = (1 - t) * startValue + t * endValue
-vec3 color(const ray &r)
+vec3 color(const ray &r, hittable *world)
 {
-    // coloring any pixel that hits a small sphere we place at −1 on the z-axis
-    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-    if (t > 0.0)
+    hit_record rec;
+    if (world->hit(r, 0.0, MAXFLOAT, rec))
     {
         // normal is in the direction of the hitpoint minus the center
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+        // map x/y/z to r/g/b
+        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
     }
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    else
+    {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
 }
 
 int main()
@@ -44,11 +48,15 @@ int main()
     int ny = 100;
     std::cout << "P3\n"
               << nx << " " << ny << "\n255\n";
-
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
+
+    hittable *list[2];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hittable *world = new hittable_list(list, 2);
 
     for (int j = ny - 1; j >= 0; j--)
     {
@@ -57,7 +65,10 @@ int main()
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 col = color(r);
+
+            vec3 p = r.point_at_parameter(2.0);
+            vec3 col = color(r, world);
+
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
